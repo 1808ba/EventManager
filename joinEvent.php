94bbd652d94c19d82a479dab81333d40e5091755
1808ba <bendoumaaziza8@gmail.com>
2024-05-18 12:11:06 +0100

    modification email

--- joinEvent.php
+++ joinEvent.php
@@ -20,8 +20,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP(); 
         $mail->Host = 'smtp.gmail.com'; 
         $mail->SMTPAuth = true; 
-        $mail->Username = 'zizibn18@gmail.com'; 
-        $mail->Password = 'jepc hqvx fuyj wtxz'; 
+        $mail->Username = ''; 
+        $mail->Password = ''; 
         $mail->SMTPSecure = 'tls'; 
         $mail->Port = 587; 
 
